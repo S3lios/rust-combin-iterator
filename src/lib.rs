2b2cfcf0d5fdd009e7e3e3e4f17210aeab7f6be0
S3lios <mael.mtrn.imp@gmail.com>
2024-01-24 18:42:03 +0100

    Update Cargo.toml, and clear src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,3 @@ pub mod alternate_iterator;
 
 #[macro_use]
 pub mod macros;
-
-
-fn main() {
-    let vec1 = vec![1.0];
-    let vec2 = vec![1.0];
-    let vec3 = vec![2.0];
-    for k in 0..1 {
-        print!("{:#?}", k);
-    }
-}
-
-
-#[cfg(test)]
-mod tests {
-}
